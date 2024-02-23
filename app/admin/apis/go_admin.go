2d1ce62a06ee63adb0be03ae5bededad6331fed9
zhaichaoqun <1047738527@qq.com>
2024-02-24 01:27:02 +0800

    fix

--- app/admin/apis/go_admin.go
+++ app/admin/apis/go_admin.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-admin-team/go-admin-core/sdk/api"
-	"github.com/go-admin-team/go-admin-core/sdk/pkg/captcha"
+	"github.com/google/uuid"
+	"github.com/mojocn/base64Captcha"
 	"go-admin/common/global"
 )
-
+type configJsonBody struct {
+	Id            string
+	CaptchaType   string
+	VerifyValue   string
+	DriverAudio   *base64Captcha.DriverAudio
+	DriverString  *base64Captcha.DriverString
+	DriverChinese *base64Captcha.DriverChinese
+	DriverMath    *base64Captcha.DriverMath
+	DriverDigit   *base64Captcha.DriverDigit
+}
 type GoAdminSystem struct {
 	api.Api
 }
 func (e GoAdminSystem)GoAdmin(c *gin.Context) {
 	c.String(200, fmt.Sprintf( "dcy-store %s",global.Version))
 }
+func DriverDigitFunc() (id, b64s string, err error) {
+	e := configJsonBody{}
+	e.Id = uuid.New().String()
 
+	e.DriverDigit = base64Captcha.NewDriverDigit(80, 240, 4, 0.2, 16)
+	driver := e.DriverDigit
+
+	return base64Captcha.NewCaptcha(driver, base64Captcha.DefaultMemStore).Generate()
+}
 func (e GoAdminSystem)GenerateCaptchaHandler(c *gin.Context) {
 
 	err := e.MakeContext(c).Errors
@@ -22,7 +40,7 @@ func (e GoAdminSystem)GenerateCaptchaHandler(c *gin.Context) {
 		e.Error(500, err, "服务初始化失败！")
 		return
 	}
-	id, b64s, err := captcha.DriverDigitFunc()
+	id, b64s, err := DriverDigitFunc()
 	if err != nil {
 		e.Logger.Errorf("DriverDigitFunc error, %s", err.Error())
 		e.Error(500, err, "验证码获取失败")
